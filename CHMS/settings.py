d311c5142c34689a20fc8edd5268f0c8c8c06c33
Arun Sharma <arunm22nov>
2021-02-24 06:11:01 +0530

    code commi 223

--- CHMS/settings.py
+++ CHMS/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=[BASE_DIR / 'assets/',]
+STATIC_ROOT=[BASE_DIR / 'assets',]
 
 STATICFILES_DIRS=[BASE_DIR / 'static',]
 django_heroku.settings(locals())
